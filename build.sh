357b53e637886573a54a98985ea6a128f2f8ce90
Jacob Francis <jfrancis537@gmail.com>
2023-12-17 14:12:18 -0800

    updated build script

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
+echo "Getting Ready..."
 rm -rf ./publish
 mkdir -p ./publish/public
+echo "Fetching Dependencies..."
+yarn
+echo "Building..."
 yarn build
+echo "Cleaning up..."
 rm -rf ./server/tsc_out
 cp -r ./client/dist/* ./publish/public
 rm -rf ./client/dist
